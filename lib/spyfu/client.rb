677f69dae24673672cc87d7ca276f22affa730a2
Reid Workman <workman@internetfaction.com>
2017-09-24 23:22:26 -0400

    Reference new API classes

--- lib/spyfu/client.rb
+++ lib/spyfu/client.rb
@@ -7,28 +7,28 @@ module SpyFu
       @secret_key = secret_key
     end
 
-    def kss_api
-      SpyFu::Api::Kss.new(self)
-    end
-
-    def weekly_tracking_api
-      SpyFu::Api::WeeklyTracking.new(self)
-    end
-
-    def url_api
-      SpyFu::Api::Url.new(self)
-    end
-
-    def leads_api
-      SpyFu::Api::Leads.new(self)
-    end
-
     def ad_history_api
       SpyFu::Api::AdHistory.new(self)
     end
 
     def core_api
       SpyFu::Api::Core.new(self)
+    end
+
+    def kss_api
+      SpyFu::Api::Kss.new(self)
+    end
+
+    def leads_api
+      SpyFu::Api::Leads.new(self)
+    end
+
+    def url_api
+      SpyFu::Api::Url.new(self)
+    end
+
+    def weekly_tracking_api
+      SpyFu::Api::WeeklyTracking.new(self)
     end
   end
 end
